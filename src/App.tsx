9d39313325a70a56bcd8930fed9f7b501a6ea6c2
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-07-27 12:09:34 +0600

    initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl bg-customGray">Hello world</h1>
+      <h1 className="text-4xl bg-customApricot">Hello world</h1>
+      <button className="btn bg-customAntiqueWhite">Secondary</button>
+    </>
+  );
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customGray: "#EEF2F3",
+        customApricot: "#FFE4C9",
+        customAntiqueWhite: "#F4E9E5",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["cupcake"],
+  },
+};

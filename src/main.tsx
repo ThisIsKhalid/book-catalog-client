18aaa2816de1226b79e664916d82ccbe13fe155a
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-07-29 14:48:41 +0600

    installed redux toolkit, set up store, hook, and apiSlice

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+import store from "./redux/store.ts";
 import router from "./routes/routes.tsx";
- import { ToastContainer } from "react-toastify";
- import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ToastContainer autoClose={2000} />
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <ToastContainer autoClose={2000} />
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );

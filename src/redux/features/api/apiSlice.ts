051c911c2af4dfd53a4c7a77a6305d9259ee89f8
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-08-02 22:53:52 +0600

    added readme file

--- src/redux/features/api/apiSlice.ts
+++ src/redux/features/api/apiSlice.ts
@@ -3,8 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    // baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://book-catalog-server-khaki.vercel.app/api/v1",
   }),
-  tagTypes: ['books', 'reviews', 'users'],
+  tagTypes: ["books", "reviews", "users"],
   endpoints: () => ({}),
 });
